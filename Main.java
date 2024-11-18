432c817de31cf69be291fbe3fd4c1b521ea60a26
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-18 18:52:20 +0000

    h

--- Main.java
+++ Main.java
@@ -4,11 +4,18 @@ public class Main
 {
   public static void main(String[] args)
   {
+    printNTimes("code", 3);
     // Make sure to test your methods here
   }
 
   public static void printNTimes(String word, int N)
   {
+    for(int i = 0; i < word.length(); i++) {
+        String letter = word.substring(i, i + 1);
+        for(int f = 0; f < N; f++) {
+            System.out.print(letter);
+        }
+    }
     // code solution here
   }
 
