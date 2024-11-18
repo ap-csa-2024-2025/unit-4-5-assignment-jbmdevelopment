613c96448d510dd60bfb7e7a2730015b7cfbf3d8
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-18 19:10:02 +0000

    h

--- Main.java
+++ Main.java
@@ -10,11 +10,21 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
+    // #1
     for(int i = 0; i < word.length(); i++) {
         String letter = word.substring(i, i + 1);
         for(int f = 0; f < N; f++) {
             System.out.print(letter);
         }
+    }
+    // #2
+    System.out.println("");
+    for(int start = 10; start > 0; start--) {
+      
+      for(int times = 0; times < start; times++) {
+          System.out.print(" " + start); // add a space (use start instead of times).
+      }
+      System.out.println("");
     }
     // code solution here
   }
